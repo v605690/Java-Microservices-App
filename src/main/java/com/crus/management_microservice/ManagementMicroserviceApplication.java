dcc69a2e702f1d162e6bb5f84928f5e06a2dc09f
v605690 <v605690@gmail.com>
2025-09-21 09:16:39 -0500

    Setup eureka server

--- src/main/java/com/crus/management_microservice/ManagementMicroserviceApplication.java
+++ src/main/java/com/crus/management_microservice/ManagementMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.crus.management_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ManagementMicroserviceApplication {
 
 	public static void main(String[] args) {
