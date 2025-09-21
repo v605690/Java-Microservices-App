56dfd907969c62760a0cb88aae84442a03ff1932
v605690 <v605690@gmail.com>
2025-09-21 09:12:32 -0500

    Add new Java microservices project

---
+++ src/main/java/com/crus/management_microservice/ManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.crus.management_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementMicroserviceApplication.class, args);
+	}
+
+}
